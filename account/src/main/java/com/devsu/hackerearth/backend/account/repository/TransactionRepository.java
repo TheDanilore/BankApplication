d1c6523544c9b273b8d5094b44d8a0fbe0046e43
TheDanilore <arteagagordillol@gmail.com>
2025-02-27 20:53:50 -0500

    update

--- account/src/main/java/com/devsu/hackerearth/backend/account/repository/TransactionRepository.java
+++ account/src/main/java/com/devsu/hackerearth/backend/account/repository/TransactionRepository.java
@@ -12,9 +12,9 @@ import com.devsu.hackerearth.backend.account.model.Transaction;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    @Query("SELECT t FROM Transaction t WHERE t.accountId IN (SELECT a.id FROM Account a WHERE a.clientId = :clientId) AND t.date BETWEEN :startDate AND :endDate")
+    @Query("SELECT t FROM Transaction t WHERE t.accountId IN (SELECT a.id FROM Account a WHERE a.clientId = :clientId) AND t.date BETWEEN :dateTransactionStart AND :dateTransactionEnd")
     List<Transaction> findByAccountClientIdAndDateBetween(@Param("clientId") Long clientId,
-            @Param("startDate") Date startDate, @Param("endDate") Date endDate);
+            @Param("dateTransactionStart") Date dateTransactionStart, @Param("dateTransactionEnd") Date dateTransactionEnd);
 
     @Query("SELECT t FROM Transaction t WHERE t.accountId = :accountId ORDER BY t.date DESC")
     Optional<Transaction> findTopByAccountIdOrderByDateDesc(@Param("accountId") Long accountID);
